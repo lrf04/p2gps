1dc6a52359b7ca7e40a74806c5b7ae61ca1aed7e
luis ruiz flores <luis.ruiz2@alu.uclm.es>
2021-11-16 18:30:53 +0100

    [ci skip]

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-while ! curl http://mongo:27017/
-do
-  echo "$(date) - still trying"
-  sleep 1
-done
-echo "$(date) - connected successfully"
-
-java -jar ordering-*.jar
+mongodb.host: mongo
+mongodb.database: ordering
